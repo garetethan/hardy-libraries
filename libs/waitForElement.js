0b137e8a1009119966aadd53d482dcfaf6a5a81e
Hardy <blahblahbrian@protonmail.com>
2023-09-01 23:16:36 +0530

    Update waitForElement.js

--- libs/waitForElement.js
+++ libs/waitForElement.js
@@ -1,13 +1,23 @@
-function waitForElement(selector) {
-    return new Promise(resolve => {
-        function tempFunc() {
-            let element = document.querySelector(selector);
-            if (element) {
-                return resolve(element);
-            } else {
-                setTimeout(tempFunc, 1000);
-            }
-        }
-        tempFunc();
-    });
-}
+let waitObj = {};
+function waitForElement(selector, duration, maxTries, identifier) {
+        return new Promise(function(resolve, reject) {
+            const value = Math.floor(Math.random() * 1000000000);
+            waitObj[identifier] = value;
+            let attempts = 0;
+            const intervalId = setInterval(() => {
+                if (attempts > maxTries){
+                    clearInterval(intervalId);
+                    reject(`Selector Listener Expired: ${selector}, Reason: Dead bcoz u didnt cum on time!!!!`);
+                } else if (waitObj[identifier] !== value) {
+                    clearInterval(intervalId);
+                    reject(`Selector Listener Expired: ${selector}, Reason: Dead coz u didnt luv me enough and got another SeLecTor!!!!`);
+                }
+                const element = document.querySelector(selector);
+                if (element) {
+                    clearInterval(intervalId);
+                    resolve(element);
+                }
+                attempts += 1;
+            }, duration);
+        });
+    }
