93f5f9a62e92e65839e974f3a24095f293b03710
Hardy <blahblahbrian@protonmail.com>
2023-09-20 07:24:06 +0530

    Update pref.js

--- libs/pref.js
+++ libs/pref.js
@@ -1,13 +1,29 @@
-let saved = JSON.parse(GM_getValue("prefs", `{"checkbox": {}, "input": {}, "select": {}}`));
+// ==UserScript==
+// @name         CE: Quick Links
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://cartelempire.online/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=cartelempire.online
+// @grant        GM_getValue
+// @grant        GM_setValue
+// @grant        GM_addStyle
+// @grant        GM_registerMenuCommand
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    let saved = JSON.parse(GM_getValue("prefs", `{"checkbox": {}, "input": {}, "select": {}}`));
     const options = {"checkbox": {"option1": {"name": "Selection Option 1", "def": "yes"}, "option2": {"name": "Selection Option 2", "def": "no"}}, "input": {"api_key": {"name": "Enter Your APIkey here", "def": "N/A"}, "booobies": {"name": "Show your boobies", "def": "I will nots"}}, "select": {"car_pref": {"name": "Choose your fav car", "def": "rr", "options": [["bmw", "B M W"], ["audi", "AUDI"], ["rr", "Rolls Royce"]]}}};
     GM_registerMenuCommand("Preferences", ()=> {
-        createPreferencesBox();
+        createPreferencesBox('#hardy_pref_box_q-links');
     });
-    function createPreferencesBox() {
-        const prefBox = document.querySelector('#hardy_pref_box_q-links');
+    function createPreferencesBox(selector) {
+        const prefBox = document.querySelector(selector);
         if (!prefBox) {
-            const box = createElement("div", {"id": "hardy_pref_box_q-links", "class": "hardy_modal_dialog"});
-            box.innerHTML = `<div class="hardy_modal"><div class="hardy_modal_header"><label>Preferences</label></div><div style="text-align: right;"><button class="hardy_modal_close">Close</button></div><div style="overflow: auto;"><div class="hardy_modal_content"></div></div></div>`;
+            const box = createElement("div", {"id": selector, "class": "hardy_modal_dialog"});
+            box.innerHTML = `<div class="hardy_modal"><div class="hardy_modal_header"><label>Preferences</label></div><div style="text-align: right;"><button class="hardy_modal_close">Close</button></div><div style="overflow: auto;"><div class="hardy_modal_content"></div><div class="hardy_modal_msg"></div></div></div>`;
             box.querySelector(".hardy_modal_close").addEventListener("click", ()=> {
                 box.remove();
             });
@@ -61,47 +77,33 @@ let saved = JSON.parse(GM_getValue("prefs", `{"checkbox": {}, "input": {}, "sele
                     saved.select[name] = select.value;
                 }
                 GM_setValue("prefs", JSON.stringify(saved));
+                box.querySelector(".hardy_modal_msg").innerHTML = `Preferences have been saved successfully!!`;
             });
         }
     }
     function preferenceHandler(inpType, name, def, opt) {
-        if (inpType === "checkbox") {
-            if (saved.checkbox[name]) {
-                if (saved.checkbox[name] === "yes") {
-                    return " checked";
+        switch (inpType) {
+            case "checkbox":
+                if (saved.checkbox[name]) {
+                    return saved.checkbox[name] === "yes" ? " checked" : "";
                 } else {
-                    return "";
+                    saved.checkbox[name] = def;
+                    return def === "yes" ? " checked" : "";
                 }
-            } else {
-                saved.checkbox[name] = def;
-                if (def === "yes") {
-                    return " checked";
+            case "input":
+                if (saved.input[name]) {
+                    return saved.input[name];
                 } else {
-                    return "";
+                    saved.input[name] = def;
+                    return def;
                 }
-            }
-        } else if (inpType === "input") {
-            if (saved.input[name]) {
-                return saved.input[name];
-            } else {
-                saved.input[name] = def;
-                return def;
-            }
-        } else if (inpType === "select") {
-            if (saved.select[name]) {
-                if (saved.select[name] !== "N/A" && saved.select[name] === opt) {
-                    return " selected";
+            case "select":
+                if (saved.select[name]) {
+                    return saved.select[name] !== "N/A" && saved.select[name] === opt ? " selected" : "";
                 } else {
-                    return "";
+                    saved.select[name] = def;
+                    return opt === def ? " selected" : "";
                 }
-            } else {
-                saved.select[name] = def;
-                if (opt === def) {
-                    return " selected";
-                } else {
-                    return "";
-                }
-            }
         }
     }
     function createElement(tagName, attributes) {
@@ -111,4 +113,5 @@ let saved = JSON.parse(GM_getValue("prefs", `{"checkbox": {}, "input": {}, "sele
         }
         return element;
     }
-    GM_addStyle(`.hardy_modal_dialog{position:fixed;z-index:10211;padding-top:6px;left:0;top:0;width:100%;height:100%;background-color:rgba(0,0,0,.4)}.hardy_modal{position:absolute;top:40%;left:50%;height:70%;transform:translate(-50%,-50%);background-color:#fff;width:30rem;border-radius:.5rem;overflow:auto}.hardy_modal_close{padding:2px 5px;text-align:right;background-color:#b06b6b;border-radius:2px;margin:5px;color:#fff}.hardy_modal_content label{margin:5px 0}.hardy_modal_content input[type=checkbox]{margin:5px;padding:4px}.hardy_modal_content input[type=text]{border-radius:3px;padding:3px;display:block;width:80%}.hardy_modal_content select{padding:6px;font-size:16px;border-radius:4px;margin:6px 4px}.hardy-save-prefs{background-color:#008000e8;color:#fff;display:block;padding:4px 9px;border-radius:4px}.hardy_modal_content{margin:4px;display:block;padding:7px}.hardy_modal_header{background-color:#000;text-align:center;color:#fff;border-radius:6px 6px 0 0;padding:5px;width:100%}`);
+    GM_addStyle(`.hardy_modal_dialog{position:fixed;z-index:10211;padding-top:6px;left:0;top:0;width:100%;height:100%;background-color:rgba(0,0,0,.4)}.hardy_modal{position:absolute;top:40%;left:50%;height:70%;transform:translate(-50%,-50%);background-color:#fff;width:30rem;border-radius:.5rem;overflow:auto}.hardy_modal_close{padding:2px 5px;text-align:right;background-color:#b06b6b;border-radius:2px;margin:5px;color:#fff}.hardy_modal_content label{margin:5px 0}.hardy_modal_content input[type=checkbox]{margin:5px;padding:4px}.hardy_modal_content input[type=text]{border-radius:3px;padding:3px;display:block;width:80%}.hardy_modal_content select{padding:6px;font-size:16px;border-radius:4px;margin:6px 4px}.hardy-save-prefs{background-color:#008000e8;color:#fff;display:block;padding:4px 9px;border-radius:4px}.hardy_modal_content{margin:4px;display:block;padding:7px}.hardy_modal_header{background-color:#000;text-align:center;color:#fff;border-radius:6px 6px 0 0;padding:5px;width:100%} .hardy_modal_msg {text-align: center;}`);
+})();
