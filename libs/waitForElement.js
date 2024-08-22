834767e0566f277c4cb27bac190fe90531f94c3a
Hardy <blahblahbrian@protonmail.com>
2024-08-22 19:05:23 +0530

    Add QuerySelectorAll

--- libs/waitForElement.js
+++ libs/waitForElement.js
@@ -1,23 +1,41 @@
-let waitObj = {};
-function waitForElement(selector, duration, maxTries, identifier) {
-        return new Promise(function(resolve, reject) {
-            const value = Math.floor(Math.random() * 1000000000);
-            waitObj[identifier] = value;
-            let attempts = 0;
-            const intervalId = setInterval(() => {
-                if (attempts > maxTries){
-                    clearInterval(intervalId);
-                    reject(`Selector Listener Expired: ${selector}, Reason: Dead bcoz u didnt cum on time!!!!`);
-                } else if (waitObj[identifier] !== value) {
-                    clearInterval(intervalId);
-                    reject(`Selector Listener Expired: ${selector}, Reason: Dead coz u didnt luv me enough and got another SeLecTor!!!!`);
-                }
-                const element = document.querySelector(selector);
-                if (element) {
-                    clearInterval(intervalId);
-                    resolve(element);
-                }
-                attempts += 1;
-            }, duration);
-        });
+let head = false;
+let headTimer = setInterval(() => {
+    if (document.head) {
+        head = true;
+        clearInterval(headTimer);
     }
+}, 300);
+let waitObj = {};
+
+function waitForElement(selector, duration, maxTries, identifier, allOrOne = 0) {
+    return new Promise(function(resolve, reject) {
+        const value = Math.floor(Math.random() * 1000000000);
+        waitObj[identifier] = value;
+        let attempts = 0;
+        const intervalId = setInterval(() => {
+            if (attempts > maxTries) {
+                clearInterval(intervalId);
+                reject(`Selector Listener Expired: ${selector}, Reason: Dead bcoz u didnt cum on time!!!!`);
+            } else if (waitObj[identifier] !== value) {
+                clearInterval(intervalId);
+                reject(`Selector Listener Expired: ${selector}, Reason: Dead coz u didnt luv me enough and got another SeLecTor!!!!`);
+            }
+            if (head) {
+                if (allOrOne === 0) {
+                    const element = document.querySelector(selector);
+                    if (element) {
+                        clearInterval(intervalId);
+                        resolve(element);
+                    }
+                } else if (allOrOne === 1) {
+                    const element = document.querySelectorAll(selector);
+                    if (element.length > 0) {
+                        clearInterval(intervalId);
+                        resolve(element);
+                    }
+                }
+            }
+            attempts += 1;
+        }, duration);
+    });
+}
